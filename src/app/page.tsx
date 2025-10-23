216f73e1f0d9ef9f192487028ceee078957ee5a6
Maksym Krasnov <maksimkrasnov747@gmail.com>
2025-10-23 15:43:28 +0200

    feat: add middleware for dashboard access control and create initial Dashboard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 export default function Home() {
-  
   return (
-    <h1>Лютік bot</h1>
+    <>
+      <h1>Лютік bot</h1>
+      <a href="http://localhost:3000/auth/twitch/login">Log in with Twitch</a>
+    </>
   );
 }
---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const sid = req.cookies.get("sid")?.value;
+  if (req.nextUrl.pathname.startsWith("/dashboard") && !sid) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/login"
+    return NextResponse.redirect(url)
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path"]
+}
